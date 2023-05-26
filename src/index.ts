feb68f5cf21489c99146c81dd4f1ba6394391120
zwkang <kang95630@gmail.com>
2023-05-26 15:54:32 +0800

    feat: init rollup plugin templates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const name = 'pkg-name';
+import { createFilter } from 'rollup-pluginutils';
+
+import { Plugin } from 'rollup';
+
+type Options = {
+  exclude?: string | RegExp | (string | RegExp)[];
+  include?: string | RegExp | (string | RegExp)[];
+};
+
+export default function myPlugin(options: Options = {}): Plugin {
+  const filter = createFilter(options.include, options.exclude);
+  return {
+    name: 'pkg-name',
+    load(id) {
+      if (!filter(id)) return null;
+    },
+    transform(code, id) {
+      if (!filter(id)) return null;
+    },
+  };
+}
